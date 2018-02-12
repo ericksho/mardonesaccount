b62a8b6b420a5635fe5e8b76ae3e6da8384c5b13
Erick Svec <ericksho@gmail.com>
2018-02-12 13:18:08 -0300

    agregamos Boks bundle

---
+++ src/BooksBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace BooksBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('BooksBundle:Default:index.html.twig');
+    }
+}
